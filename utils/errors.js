bb07e7da586fe61ec5582576b6acf0848030a5b7
Imani Walker <imaniwalker0305@gmail.com>
2024-08-03 23:20:49 -0400

    Cleaning up code un used code

--- utils/errors.js
+++ utils/errors.js
@@ -1,9 +1,3 @@
-// export const BAD_REQUEST = 400;
-// export const NOT_FOUND = 404;
-// export const DEFAULT = 500;
-// export const OKAY_REQUEST = 200;
-// export const CREATE_REQUEST = 201;
-
 module.exports = {
     BAD_REQUEST: 400,
     NOT_FOUND: 404,
