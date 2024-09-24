47970c0c6dff4375ecc0e1542a6144072c61626a
Imani Walker <imaniwalker0305@gmail.com>
2024-09-24 16:58:41 -0400

    appjs update

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # WTWR (What to Wear?): Back End
+
 The back-end project is focused on creating a server for the WTWR application.
- 
+domain name www.what-to-wear.happyforever.com
--- app.js
+++ app.js
@@ -15,6 +15,11 @@ mongoose
 app.use(express.json());
 
 app.use(cors());
+app.get("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Server will crash now");
+  }, 0);
+});
 app.post("/signin", logIn);
 app.post("/signup", createUser);
 
