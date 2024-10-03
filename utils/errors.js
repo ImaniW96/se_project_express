97653e824d9ea35c8cfef742cffe4094a44b9f16
Imani Walker <imaniwalker0305@gmail.com>
2024-10-03 00:28:12 -0400

    Error update

--- utils/errors.js
+++ utils/errors.js
@@ -8,3 +8,62 @@ module.exports = {
   CREATE_REQUEST: 201,
   DUPLICATE_ERROR: 409,
 };
+
+class BadRequestError extends Error {
+  constructor(message) {
+    super(message);
+    this.statusCode = 400;
+  }
+}
+class FrobiddenError extends Error {
+  constructor(message) {
+    super(message);
+    this.statusCode = 403;
+  }
+}
+class NotFound extends Error {
+  constructor(message) {
+    super(message);
+    this.statusCode = 404;
+  }
+}
+class Default extends Error {
+  constructor(message) {
+    super(message);
+    this.statusCode = 500;
+  }
+}
+class NotAuthorized extends Error {
+  constructor(message) {
+    super(message);
+    this.statusCode = 401;
+  }
+}
+class OkayRequest extends Error {
+  constructor(message) {
+    super(message);
+    this.statusCode = 200;
+  }
+}
+class CreateRequest extends Error {
+  constructor(message) {
+    super(message);
+    this.statusCode = 201;
+  }
+}
+class DuplicateError extends Error {
+  constructor(message) {
+    super(message);
+    this.statusCode = 409;
+  }
+}
+module.exports = {
+  BadRequestError,
+  FrobiddenError,
+  NotFound,
+  Default,
+  NotAuthorized,
+  OkayRequest,
+  CreateRequest,
+  DuplicateError,
+};
